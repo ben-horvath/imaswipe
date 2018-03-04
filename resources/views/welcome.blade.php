d775539ce582de58d3f5c5093c4fa585724b077c
Ben <dev.ben.h@gmail.com>
2018-03-04 10:55:12 +0100

    Revert "Setup basic layout"
    This reverts commit de6ca21d77c53c469ab96457fc9b824016732f4d.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,24 +5,24 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ env('APP_NAME') }}</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #000;
-                height: 100%;
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
                 margin: 0;
             }
 
             .full-height {
-                height: 100%;
-            }
-
-            .full-width {
-                width: 100%;
+                height: 100vh;
             }
 
             .flex-center {
@@ -35,21 +35,61 @@
                 position: relative;
             }
 
-            .max-full-height {
-                max-height: 100%;
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
             }
 
-            .max-full-width {
-                max-width: 100%;
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
             }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height full-width">
-            <img
-                class="max-full-height max-full-width"
-                src="{{ asset('storage/OXHPI6MpqdbrI2pcFer7dveCW3pzX9RvMpAhj9Hz.jpeg') }}"
-            >
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
         </div>
     </body>
 </html>
