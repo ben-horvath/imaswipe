7f0f609af74af8675ae4f856d20507906e681671
Ben <dev.ben.h@gmail.com>
2019-08-14 16:03:01 +0200

    Send upgrade events to Google Analytics

--- resources/js/upgrade.js
+++ resources/js/upgrade.js
@@ -1,5 +1,15 @@
 function upgrade(amount) {
     axios.post('/upgrade', {amount: amount});
+
+    let action = '';
+
+    if (amount > 0) {
+        action = 'upgrade';
+    } else {
+        action = 'noUpgrade';
+    }
+
+    ga('send', 'event', 'Upgrade', action, '', amount);
 }
 
 export {
