ae6e219c3374fc89c5a8d7022e8b86d19108e4c1
Ben <dev.ben.h@gmail.com>
2019-12-13 23:31:15 +0100

    Create a simple test

---
+++ tests/Feature/Http/Controllers/WelcomeControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
+
+class WelcomeControllerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function permalink_passes_the_medium_id_to_view()
+    {
+        $medium_id = Str::random(40);
+
+        $response = $this->get(route('welcome-start-with', [$medium_id]));
+
+        $response->assertStatus(200);
+
+        $response->assertViewIs('welcome');
+
+        $response->assertViewHas('first_medium', $medium_id);
+    }
+
+    /**
+     * @test
+     */
+    public function permalink_medium_id_does_not_exist()
+    {
+        $this->markTestIncomplete();
+
+        $medium_id = Str::random(20);
+
+        $response = $this->get(route('welcome-start-with', [$medium_id]));
+
+        $response->assertRedirect(route('welcome'));
+    }
+}
